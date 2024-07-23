3eb18a9918198546337d47b8d7bfd127a77f836c
Daniela Novoa Ramirez <66880047+DanielaNovoa15@users.noreply.github.com>
2024-07-22 23:26:02 -0500

    Update StochasticEvents.py

--- StochasticEvents.py
+++ StochasticEvents.py
@@ -14,7 +14,7 @@ from PIL import Image, ImageTk
 # ........ Directory Library ..................................................
 import os
 # -------- Own libraries ------------------------------------------------------
-import FuntionsLibrary as wnfun_lib
+import FunctionsLibrary as wnfun_lib
 
 #%% ====== SHOW EVENTS ELEMENTS =================================================
 def Show_Events_Elements(EVNT_title,EVNT_text,EVNT_label,EVNT_boton,cnt_container,cnt_color,upcnt_color,Show_Calibrate,Show_Dispersion):   
